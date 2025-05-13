2a1e8294d65b8348ad7dbadc437f9e8bf9942ae1
Bruno Dorigon <bdorigon@gmail.com>
2025-05-13 17:07:04 -0300

    fix: fix bug in data.erro

--- src/components/Product/ShippingCalculator.tsx
+++ src/components/Product/ShippingCalculator.tsx
@@ -26,7 +26,7 @@ const ShippingCalculator = ({ productPrice, onShippingChange }: Props) => {
       const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
       const data: Address = await response.json();
 
-      if (data.erro) {
+      if ("erro" in data && data.erro) {
         throw new Error("CEP não encontrado");
       }
 
